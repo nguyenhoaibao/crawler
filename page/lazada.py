9850f341795d36bd5f104f95c6a50392ea577239
baonh <nhbao.la@gmail.com>
2014-08-28 15:38:32 +0700

    do not insert duplicate product id

--- page/lazada.py
+++ page/lazada.py
@@ -97,7 +97,7 @@ def parse_url(url):
 			}
 		
 			#insert data to mongo
-			mongo_collection.insert(product_data)
+			mongo_collection.update({'product_id' : int(product_id)}, product_data, {upsert: true})
 	except Exception, e:
 		print url, str(e.args)
 	
